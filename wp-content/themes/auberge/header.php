cea2115b2a0a8d8465546edf209eef81c0bc3545
jddoesdev <jd@jamesdflynn.com>
2017-01-25 15:43:36 -0600

    first commit

---
+++ wp-content/themes/auberge/header.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Website header template
+ *
+ * @package    Auberge
+ * @copyright  WebMan Design, Oliver Juhas
+ *
+ * @since    1.0
+ * @version  2.0
+ */
+
+
+
+
+
+/**
+ * HTML
+ */
+
+	do_action( 'tha_html_before' );
+
+?>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+<head>
+
+<?php
+
+/**
+ * HTML head
+ */
+
+echo '<!-- tha head top -->';
+
+	do_action( 'tha_head_top' );
+echo '<!-- tha head bottom -->';
+	do_action( 'tha_head_bottom' );
+echo '<!-- tha head -->';
+	wp_head();
+
+?>
+
+</head>
+
+
+<body id="top" <?php body_class(); echo wm_schema_org( 'WebPage' ); ?>>
+
+<?php
+
+/**
+ * Body
+ */
+
+	do_action( 'tha_body_top' );
+
+
+
+/**
+ * Header
+ */
+
+	if ( ! apply_filters( 'wmhook_disable_header', false ) ) {
+
+		do_action( 'tha_header_before' );
+
+		do_action( 'tha_header_top' );
+
+		do_action( 'tha_header_bottom' );
+
+		do_action( 'tha_header_after' );
+
+	} // /wmhook_disable_header
+
+
+
+/**
+ * Content
+ */
+
+	do_action( 'tha_content_before' );
+
+	do_action( 'tha_content_top' );
