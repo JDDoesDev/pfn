f1a7a9bf7106e68aa218585c4f41859b58ca3b13
jddoesdev <jd@jamesdflynn.com>
2017-02-13 19:17:29 -0600

    image styles

--- wp-content/themes/x-child/functions.php
+++ wp-content/themes/x-child/functions.php
@@ -23,3 +23,20 @@ add_filter( 'x_enqueue_parent_stylesheet', '__return_true' );
 // Additional Functions
 // =============================================================================
 
+// Make sure featured images are enabled
+add_theme_support( 'post-thumbnails' );
+
+// Add other useful image sizes for use through Add Media modal
+add_image_size( 'medium-width', 480 );
+add_image_size( 'medium-height', 9999, 480 );
+add_image_size( 'medium-something', 480, 480 );
+
+// Register the three useful image sizes for use in Add Media modal
+add_filter( 'image_size_names_choose', 'x_child_custom_sizes' );
+function x_child_custom_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'medium-width' => __( 'Medium Width' ),
+        'medium-height' => __( 'Medium Height' ),
+        'medium-something' => __( 'Medium Something' ),
+    ) );
+}
